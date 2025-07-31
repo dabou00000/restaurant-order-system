1620a149b1020781f7db1ed52844a4c8774de520
dabou00000 <dabou4947@gmail.com>
2025-07-31 21:05:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ function prepareOrder() {
       alert("❌ فشل اختصار الرابط. حاول لاحقًا.");
     });
 }
-
+document.getElementById("generate-customer-link").addEventListener("click", prepareOrder);
 
 function removeItem(index) {
   selectedItems.splice(index, 1);
